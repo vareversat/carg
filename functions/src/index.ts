a1501c886b26998bec726f513440e59b0c20ed9f
Valentin REVERSAT <valentin.reversat22@gmail.com>
2020-09-23 23:01:26 +0200

    Add functions for each flavor

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,31 +3,55 @@ import algoliasearch from 'algoliasearch';
 
 const ALGOLIA_APP_ID = 'A0M2ID7FCT';
 const ALGOLIA_ADMIN_KEY = 'f897240bd9ba1dbaf4cbda253903f47a';
-const ALGOLIA_INDEX_NAME = 'players';
 
 let client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_ADMIN_KEY);
-const index = client.initIndex(ALGOLIA_INDEX_NAME);
+const indexDev = client.initIndex('player_dev');
+const indexProd = client.initIndex('player_prod');
 
-export const onPlayerCreated = functions.firestore.document('player/{playerId}').onCreate((snap, context) => {
+export const onPlayerCreatedDev = functions.firestore.document('player-dev/{playerId}').onCreate((snap, context) => {
     const player = snap.data();
     if (player !== undefined) {
         player.objectID = context.params.playerId;
-        return index.saveObject(player);
+        return indexDev.saveObject(player);
     }
     return 'Cannot add to index'
 
 });
 
-export const onPlayerUpdated = functions.firestore.document('player/{playerId}').onUpdate((snap, context,) => {
+export const onPlayerUpdatedDev = functions.firestore.document('player-dev/{playerId}').onUpdate((snap, context,) => {
     const newPlayer = snap.after.data();
     if (newPlayer !== undefined) {
         newPlayer.objectID = context.params.playerId;
-        return index.saveObject(newPlayer);
+        return indexDev.saveObject(newPlayer);
     }
     return 'Cannot update index'
 
 });
 
-export const onPlayerDeleted = functions.firestore.document('player/{playerId}').onDelete((snap, context,) => {
-    return index.deleteObject(context.params.playerId);
+export const onPlayerDeletedDev = functions.firestore.document('player-dev/{playerId}').onDelete((snap, context,) => {
+    return indexDev.deleteObject(context.params.playerId);
+});
+
+export const onPlayerCreatedProd = functions.firestore.document('player-prod/{playerId}').onCreate((snap, context) => {
+    const player = snap.data();
+    if (player !== undefined) {
+        player.objectID = context.params.playerId;
+        return indexProd.saveObject(player);
+    }
+    return 'Cannot add to index'
+
+});
+
+export const onPlayerUpdatedProd = functions.firestore.document('player-prod/{playerId}').onUpdate((snap, context,) => {
+    const newPlayer = snap.after.data();
+    if (newPlayer !== undefined) {
+        newPlayer.objectID = context.params.playerId;
+        return indexProd.saveObject(newPlayer);
+    }
+    return 'Cannot update index'
+
+});
+
+export const onPlayerDeletedProd = functions.firestore.document('player-prod/{playerId}').onDelete((snap, context,) => {
+    return indexProd.deleteObject(context.params.playerId);
 });
