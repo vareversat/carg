1707ce1fbb1fa763861baceb5df762cab591da7e
vareversat <valentin.reversat@dev-o-sud.fr>
2020-10-11 18:38:39 +0200

    Implement animations for login screen

--- lib/views/screens/login_screen.dart
+++ lib/views/screens/login_screen.dart
@@ -15,7 +15,9 @@ class LoginScreen extends StatefulWidget {
   _LoginScreenState createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _LoginScreenState extends State<LoginScreen>
+    with SingleTickerProviderStateMixin {
+  AuthMode _authMode = AuthMode.Login;
   final GlobalKey<FormState> _formKey = GlobalKey();
   final _passwordController = TextEditingController();
   final Map<String, String> _authData = {
@@ -25,7 +27,8 @@ class _LoginScreenState extends State<LoginScreen> {
   };
   var _isLoginLoading = false;
   var _isLocalLoginLoading = false;
-  AuthMode _authMode = AuthMode.Login;
+  AnimationController _animationController;
+  Animation<double> _opacityAnimation;
 
   Future<void> _localLogin() async {
     setState(() {
@@ -131,11 +134,28 @@ class _LoginScreenState extends State<LoginScreen> {
       setState(() {
         _authMode = AuthMode.Signup;
       });
+      _animationController.forward();
     } else {
       setState(() {
         _authMode = AuthMode.Login;
       });
+      _animationController.reverse();
     }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 400));
+    _opacityAnimation = Tween<double>(begin: 0, end: 1).animate(
+        CurvedAnimation(parent: _animationController, curve: Curves.linear));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _animationController.dispose();
   }
 
   @override
@@ -192,11 +212,14 @@ class _LoginScreenState extends State<LoginScreen> {
                         borderRadius: BorderRadius.circular(10.0),
                       ),
                       elevation: 8.0,
-                      child: Container(
+                      child: AnimatedContainer(
+                        duration: Duration(milliseconds: 300),
+                        curve: Curves.easeIn,
+                        height: _authMode == AuthMode.Signup ? 420 : 300,
+                        width: deviceSize.width * 0.75,
                         constraints: BoxConstraints(
                             minHeight:
-                                _authMode == AuthMode.Signup ? 320 : 260),
-                        width: deviceSize.width * 0.75,
+                                _authMode == AuthMode.Signup ? 420 : 200),
                         padding: EdgeInsets.all(16.0),
                         child: Form(
                           key: _formKey,
@@ -222,26 +245,36 @@ class _LoginScreenState extends State<LoginScreen> {
                                     _authData['email'] = value;
                                   },
                                 ),
-                                if (_authMode == AuthMode.Signup)
-                                  TextFormField(
-                                    autofillHints: [AutofillHints.newUsername],
-                                    keyboardType: TextInputType.visiblePassword,
-                                    enabled: _authMode == AuthMode.Signup,
-                                    decoration: InputDecoration(
-                                      labelText: 'Nom d\'utilisateur',
-                                      prefixIcon: Icon(
-                                          FontAwesomeIcons.userCircle,
-                                          size: 20),
+                                  AnimatedContainer(
+                                    duration: Duration(milliseconds: 300),
+                                    constraints: BoxConstraints(
+                                      minHeight: _authMode == AuthMode.Signup ? 60 : 0,
+                                      maxHeight: _authMode == AuthMode.Signup ? 120 : 0
                                     ),
-                                    // ignore: missing_return
-                                    validator: (value) {
-                                      if (value.isEmpty) {
-                                        return 'Nom d`\'utilisateur non renseigné';
-                                      }
-                                    },
-                                    onSaved: (value) {
-                                      _authData['username'] = value;
-                                    },
+                                    curve: Curves.easeIn,
+                                    child: FadeTransition(
+                                      opacity: _opacityAnimation,
+                                      child: TextFormField(
+                                        keyboardType:
+                                            TextInputType.visiblePassword,
+                                        enabled: _authMode == AuthMode.Signup,
+                                        decoration: InputDecoration(
+                                          labelText: 'Nom d\'utilisateur',
+                                          prefixIcon: Icon(
+                                              FontAwesomeIcons.userCircle,
+                                              size: 20),
+                                        ),
+                                        // ignore: missing_return
+                                        validator: (value) {
+                                          if (value.isEmpty) {
+                                            return 'Nom d`\'utilisateur non renseigné';
+                                          }
+                                        },
+                                        onSaved: (value) {
+                                          _authData['username'] = value;
+                                        },
+                                      ),
+                                    ),
                                   ),
                                 TextFormField(
                                   autofillHints: [AutofillHints.password],
@@ -258,25 +291,34 @@ class _LoginScreenState extends State<LoginScreen> {
                                     _authData['password'] = value;
                                   },
                                 ),
-                                if (_authMode == AuthMode.Signup)
-                                  TextFormField(
-                                    autofillHints: [AutofillHints.password],
-                                    enabled: _authMode == AuthMode.Signup,
-                                    decoration: InputDecoration(
-                                      labelText: 'Confirmer le mot de passe',
-                                      prefixIcon:
-                                          Icon(FontAwesomeIcons.lock, size: 20),
+                                  AnimatedContainer(
+                                    duration: Duration(milliseconds: 300),
+                                    constraints: BoxConstraints(
+                                        minHeight: _authMode == AuthMode.Signup ? 60 : 0,
+                                        maxHeight: _authMode == AuthMode.Signup ? 120 : 0
                                     ),
-                                    obscureText: true,
-                                    validator: _authMode == AuthMode.Signup
-                                        // ignore: missing_return
-                                        ? (value) {
-                                            if (value !=
-                                                _passwordController.text) {
-                                              return 'Le mot de passe ne correspond pas';
-                                            }
-                                          }
-                                        : null,
+                                    curve: Curves.easeIn,
+                                    child: FadeTransition(
+                                      opacity: _opacityAnimation,
+                                      child: TextFormField(
+                                        enabled: _authMode == AuthMode.Signup,
+                                        decoration: InputDecoration(
+                                          labelText: 'Confirmer le mot de passe',
+                                          prefixIcon: Icon(FontAwesomeIcons.lock,
+                                              size: 20),
+                                        ),
+                                        obscureText: true,
+                                        validator: _authMode == AuthMode.Signup
+                                            // ignore: missing_return
+                                            ? (value) {
+                                                if (value !=
+                                                    _passwordController.text) {
+                                                  return 'Le mot de passe ne correspond pas';
+                                                }
+                                              }
+                                            : null,
+                                      ),
+                                    ),
                                   ),
                                 SizedBox(
                                   height: 20,
