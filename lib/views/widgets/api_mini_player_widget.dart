07f435f3be06c60ae87d94c93a587c378dd73550
vareversat <valentin.reversat@dev-o-sud.fr>
2020-10-12 11:38:22 +0200

    Implement Transition + support errors for api_mini_player_widget.dart

--- lib/views/widgets/api_mini_player_widget.dart
+++ lib/views/widgets/api_mini_player_widget.dart
@@ -1,6 +1,7 @@
 import 'package:carg/models/player/player.dart';
 import 'package:carg/services/player_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class APIMiniPlayerWidget extends StatefulWidget {
   final String playerId;
@@ -21,6 +22,7 @@ class _APIMiniPlayerWidgetState extends State<APIMiniPlayerWidget> {
   final PlayerService _playerService = PlayerService();
   final String _playerId;
   final bool _displayImage;
+  String _errorMessage = '';
 
   _APIMiniPlayerWidgetState(this._playerId, this._displayImage);
 
@@ -28,41 +30,72 @@ class _APIMiniPlayerWidgetState extends State<APIMiniPlayerWidget> {
   Widget build(BuildContext context) {
     return FutureBuilder<Player>(
       builder: (context, snapshot) {
+        Widget child;
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator());
-        }
-        if (snapshot.connectionState == ConnectionState.none &&
-            snapshot.hasData == null) {}
-        if (snapshot.data != null) {
-          return Center(
+          child = Center(
+            key: ValueKey(0),
             child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 2),
-              child: InputChip(
-                  avatar: (snapshot.data.profilePicture != '' && _displayImage)
-                      ? Container(
-                          decoration: BoxDecoration(
-                              border: Border.all(
-                                width: 2,
-                                color: Theme.of(context).primaryColor,
-                              ),
-                              shape: BoxShape.circle,
-                              image: DecorationImage(
-                                  image: NetworkImage(
-                                      snapshot.data.profilePicture))),
-                        )
-                      : null,
-                  onPressed: () {},
-                  label: Text(
-                    snapshot.data.userName,
-                    style: TextStyle(fontSize: 13),
-                    overflow: TextOverflow.ellipsis,
-                  )),
-            ),
+                padding: const EdgeInsets.symmetric(horizontal: 2),
+                child: InputChip(
+                    label: SpinKitThreeBounce(
+                        size: 20,
+                        itemBuilder: (BuildContext context, int index) {
+                          return DecoratedBox(
+                              decoration: BoxDecoration(
+                            color: Theme.of(context).accentColor,
+                          ));
+                        }))),
           );
         }
-        return Text('error');
+        if (snapshot.hasData) {
+          child = Center(
+            key: ValueKey(2),
+            child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 2),
+                child: InputChip(
+                    avatar:
+                        (snapshot.data.profilePicture != '' && _displayImage)
+                            ? Container(
+                                decoration: BoxDecoration(
+                                    border: Border.all(
+                                      width: 2,
+                                      color: Theme.of(context).primaryColor,
+                                    ),
+                                    shape: BoxShape.circle,
+                                    image: DecorationImage(
+                                        image: NetworkImage(
+                                            snapshot.data.profilePicture))),
+                              )
+                            : null,
+                    onPressed: () {},
+                    label: Text(snapshot.data.userName,
+                        style: TextStyle(fontSize: 13),
+                        overflow: TextOverflow.ellipsis))),
+          );
+        }
+        if (!snapshot.hasData &&
+            snapshot.connectionState == ConnectionState.done) {
+          child = Center(
+            key: ValueKey(3),
+            child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 2),
+                child: InputChip(
+                    avatar: Container(
+                        decoration: BoxDecoration(
+                      color: Theme.of(context).errorColor,
+                      shape: BoxShape.circle,
+                    )),
+                    label: Text(_errorMessage))),
+          );
+        }
+        return AnimatedSwitcher(
+            duration: Duration(milliseconds: 500), child: child);
       },
-      future: _playerService.getPlayer(_playerId),
+      future: _playerService.getPlayer(_playerId).catchError((error) => {
+            setState(() {
+              _errorMessage = error.toString();
+            })
+          }),
     );
   }
 }
