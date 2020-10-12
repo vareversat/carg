4cae57b71932355b0ade6fc11e5c287f4710d72e
vareversat <valentin.reversat@dev-o-sud.fr>
2020-10-12 11:36:28 +0200

    Code cleanup

--- lib/views/screens/splash_screen.dart
+++ lib/views/screens/splash_screen.dart
@@ -4,16 +4,12 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: SpinKitWave(
-      itemBuilder: (BuildContext context, int index) {
-        return DecoratedBox(
+    return Scaffold(body: Center(
+        child: SpinKitWave(itemBuilder: (BuildContext context, int index) {
+      return DecoratedBox(
           decoration: BoxDecoration(
-            color: Theme
-                .of(context)
-                .accentColor,
-          ),
-        );
-      },
-    )));
+        color: Theme.of(context).accentColor,
+      ));
+    })));
   }
 }
