51712bdd730968b0e4d205cc928bb670ffe42ed8
Valentin REVERSAT <valentin.reversat22@gmail.com>
2021-02-27 00:36:00 +0100

    FIX : Fix firebase exceptions code which changed

--- lib/services/custom_exception.dart
+++ lib/services/custom_exception.dart
@@ -5,29 +5,30 @@ class CustomException implements Exception {
   CustomException(String rawMessage) {
     if (rawMessage.contains('PERMISSION_DENIED')) {
       code = 'PERMISSION_DENIED';
-      message = "Erreur : Vous n'êtes pas authentifiez";
-    } else if (rawMessage.contains('ERROR_WRONG_PASSWORD')) {
+      message = "Vous n'êtes pas authentifiez";
+    } else if (rawMessage == 'wrong-password') {
       code = 'ERROR_WRONG_PASSWORD';
       message = 'Mot de passe incorrect';
-    } else if (rawMessage.contains('ERROR_USER_NOT_FOUND')) {
+    } else if (rawMessage == 'user-not-found') {
       code = 'ERROR_USER_NOT_FOUND';
       message = 'Cet utilisateur n\'existe pas';
-    } else if (rawMessage.contains('ERROR_USER_DISABLED')) {
+    } else if (rawMessage == 'user-disabled') {
       code = 'ERROR_USER_DISABLED';
       message = 'Cet utilisateur est désactivé';
-    } else if (rawMessage.contains('ERROR_TOO_MANY_REQUESTS')) {
+    } else if (rawMessage == 'too-many-requests') {
       code = 'ERROR_TOO_MANY_REQUESTS';
       message = 'Trop d\'essais de connexion infructeux';
     } else if (rawMessage.contains('ERROR_EMAIL_NOT_VALIDATED')) {
+      print(rawMessage);
       code = 'ERROR_EMAIL_NOT_VALIDATED';
       message = 'L\'email n\'est pas encore été validé';
-    } else if (rawMessage.contains('ERROR_EMAIL_ALREADY_IN_USE')) {
+    } else if (rawMessage == 'email-already-in-use') {
       code = 'ERROR_EMAIL_ALREADY_IN_USE';
       message = 'Cette adresse email est déjà utilisée';
-    } else if (rawMessage.contains('ERROR_INVALID_EMAIL')) {
+    } else if (rawMessage == 'invalid-email') {
       code = 'ERROR_INVALID_EMAIL';
       message = 'L\'email n\'est pas valide';
-    } else if (rawMessage.contains('ERROR_WEAK_PASSWORD')) {
+    } else if (rawMessage == 'weak-password') {
       code = 'ERROR_WEAK_PASSWORD';
       message = 'Le mot de passe doit au moins contenir 6 charactères';
     } else {
