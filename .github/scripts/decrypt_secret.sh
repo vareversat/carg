bde8acf2250aacc6d4868ce0831a887f33f266d1
Valentin REVERSAT <valentin.reversat22@gmail.com>
2021-02-28 17:04:35 +0100

    DEV : Encrypt the key.json file for the migration scripts

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,22 +1,23 @@
-#!/bin/sh
 
 # Decrypt google-services.json.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" --output "$GITHUB_WORKSPACE"/android/app/google-services.json "$GITHUB_WORKSPACE"/encrypted_config/google-services.json.gpg
+# Decrypt key.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" --output "$GITHUB_WORKSPACE"/migration_scripts/key.json "$GITHUB_WORKSPACE"/encrypted_config/key.json.gpg
+# Decrypt algolia.json.gpg
+tmp_algolia_config=$(gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" "$GITHUB_WORKSPACE"/encrypted_config/algolia.json.gpg)
 # Decrypt keystore.jks.gpg
 tmp_keystore=$(gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" "$GITHUB_WORKSPACE"/encrypted_config/keystore.jks.gpg)
 # Decrypt store_password.txt.gpg
 tmp_store_password=$(gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" "$GITHUB_WORKSPACE"/encrypted_config/store_password.txt.gpg)
 # Decrypt key_password.txt.gpg
 tmp_key_password=$(gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" "$GITHUB_WORKSPACE"/encrypted_config/key_password.txt.gpg)
-# Decrypt algolia.json.gpg
-tmp_algolia_config=$(gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" "$GITHUB_WORKSPACE"/encrypted_config/algolia.json.gpg)
 
 # Inject
 echo "$tmp_algolia_config" > $GITHUB_WORKSPACE/assets/config/algolia.json
-echo "$tmp_keystore" | base64 --decode > $GITHUB_WORKSPACE/android/app/keystore.jks
-echo "storePassword=$tmp_store_password" > $GITHUB_WORKSPACE/android/key.properties
-echo "keyPassword=$tmp_key_password" >> $GITHUB_WORKSPACE/android/key.properties
-echo "keyAlias=key" >> $GITHUB_WORKSPACE/android/key.properties
-echo "storeFile=./keystore.jks" >> $GITHUB_WORKSPACE/android/key.properties
+echo "$tmp_keystore" | base64 --decode > "$GITHUB_WORKSPACE"/android/app/keystore.jks
+echo "storePassword=$tmp_store_password" > "$GITHUB_WORKSPACE"/android/key.properties
+echo "keyPassword=$tmp_key_password" >> "$GITHUB_WORKSPACE"/android/key.properties
+echo "keyAlias=key" >> "$GITHUB_WORKSPACE"/android/key.properties
+echo "storeFile=./keystore.jks" >> "$GITHUB_WORKSPACE"/android/key.properties
 
 
