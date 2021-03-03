a950fbc1cda2d7bc7d8abae857b6d23287d849e1
Valentin REVERSAT <valentin.reversat22@gmail.com>
2021-03-03 23:06:34 +0100

    UI : New slash screen

--- lib/views/screens/splash_screen.dart
+++ lib/views/screens/splash_screen.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(
-        child: SpinKitWave(itemBuilder: (BuildContext context, int index) {
-      return DecoratedBox(
-          decoration: BoxDecoration(
-        color: Theme.of(context).accentColor,
-      ));
-    })));
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: 60,
+            width: 60,
+            child: SvgPicture.asset('assets/images/card_game.svg'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Démarrage...',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          Container(
+              height: 30,
+              width: 30,
+              child: CircularProgressIndicator(
+                strokeWidth: 8,
+              ))
+        ],
+      ),
+    ));
   }
 }
