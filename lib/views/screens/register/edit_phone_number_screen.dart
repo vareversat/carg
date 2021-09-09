bebe487031a833180a70215e4299b1cce864342e
vareversat <valentin.reversat@dev-o-sud.fr>
2021-09-09 19:12:09 +0200

    Set dark status bar

--- lib/views/screens/register/edit_phone_number_screen.dart
+++ lib/views/screens/register/edit_phone_number_screen.dart
@@ -1,33 +1,37 @@
 import 'package:carg/services/auth_service.dart';
 import 'package:carg/views/widgets/register/register_phone_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
 
 class EditPhoneNumberScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-            child: SingleChildScrollView(
-      child: Column(children: <Widget>[
-        SizedBox(height: 30),
-        Container(
-          height: 150,
-          child: SvgPicture.asset(
-            'assets/images/card_game.svg',
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.dark,
+      child: Scaffold(
+          body: Center(
+              child: SingleChildScrollView(
+        child: Column(children: <Widget>[
+          SizedBox(height: 30),
+          Container(
+            height: 150,
+            child: SvgPicture.asset(
+              'assets/images/card_game.svg',
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(35),
-          child: Text(
-            'Nouveau numéro de téléphone',
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-            textAlign: TextAlign.center,
+          Padding(
+            padding: const EdgeInsets.all(35),
+            child: Text(
+              'Nouveau numéro de téléphone',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              textAlign: TextAlign.center,
+            ),
           ),
-        ),
-        RegisterPhoneWidget(
-            credentialVerificationType: CredentialVerificationType.EDIT)
-      ]),
-    )));
+          RegisterPhoneWidget(
+              credentialVerificationType: CredentialVerificationType.EDIT)
+        ]),
+      ))),
+    );
   }
 }
--- lib/views/screens/splash_screen.dart
+++ lib/views/screens/splash_screen.dart
@@ -1,34 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            height: 60,
-            width: 60,
-            child: SvgPicture.asset('assets/images/card_game.svg'),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'Démarrage...',
-              style: TextStyle(fontWeight: FontWeight.bold),
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.dark,
+      child: Scaffold(
+          body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 60,
+              width: 60,
+              child: SvgPicture.asset('assets/images/card_game.svg'),
             ),
-          ),
-          Container(
-              height: 30,
-              width: 30,
-              child: CircularProgressIndicator(
-                strokeWidth: 8,
-              ))
-        ],
-      ),
-    ));
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Démarrage...',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+                height: 30,
+                width: 30,
+                child: CircularProgressIndicator(
+                  strokeWidth: 8,
+                ))
+          ],
+        ),
+      )),
+    );
   }
 }
