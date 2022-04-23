571a5877f84af9e4e3d09dc8e985caa4ce8fceb7
Valentin REVERSAT <dev@vareversat.fr>
2022-04-24 00:10:43 +0200

    clean(contree_belote_score_repository): finish the implementation with tests

---
+++ lib/repositories/score/abstract_contree_belote_score_repository.dart
@@ -0,0 +1,12 @@
+import 'package:carg/models/score/contree_belote_score.dart';
+import 'package:carg/repositories/score/abstract_belote_score_repository.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+abstract class AbstractContreeBeloteScoreRepository
+    extends AbstractBeloteScoreRepository<ContreeBeloteScore> {
+  AbstractContreeBeloteScoreRepository(
+      {required String database,
+      required String environment,
+      required FirebaseFirestore provider})
+      : super(database: database, environment: environment, provider: provider);
+}
