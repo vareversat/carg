0489e6e55d1322a209f0a690c9149530a8749b5d
Valentin REVERSAT <dev@vareversat.fr>
2022-04-24 20:27:46 +0200

    clean(tarot_score_services): finish the implementations with tests

---
+++ lib/services/score/abstract_tarot_score_service.dart
@@ -0,0 +1,13 @@
+import 'package:carg/models/score/tarot_score.dart';
+import 'package:carg/repositories/score/abstract_tarot_score_repository.dart';
+import 'package:carg/services/score/abstract_score_service.dart';
+
+abstract class AbstractTarotScoreService
+    extends AbstractScoreService<TarotScore> {
+
+  final AbstractTarotScoreRepository tarotScoreRepository;
+
+  AbstractTarotScoreService(
+      {required this.tarotScoreRepository})
+      : super(scoreRepository: tarotScoreRepository);
+}
