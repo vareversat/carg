3d1a857fc0617713bf529db3772c327e89c51938
Valentin REVERSAT <dev@vareversat.fr>
2022-04-24 11:45:32 +0200

    clean(base_repository): finish the implementation with tests

--- lib/repositories/base_repository.dart
+++ lib/repositories/base_repository.dart
@@ -2,15 +2,18 @@ import 'package:carg/exceptions/repository_exception.dart';
 import 'package:carg/models/carg_object.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-abstract class BaseAbstractRepository<T extends CargObject> {
+abstract class BaseRepository<T extends CargObject> {
   final String database;
   final String environment;
   final FirebaseFirestore provider;
+  late final String connectionString;
 
-  BaseAbstractRepository(
+  BaseRepository(
       {required this.database,
       required this.environment,
-      required this.provider});
+      required this.provider}) {
+    connectionString = '$database-$environment';
+  }
 
   /// Get a [T] object from the database
   /// Take an [id] and return a [T]
@@ -19,7 +22,24 @@ abstract class BaseAbstractRepository<T extends CargObject> {
   /// Delete a [T] object from the database
   /// Take an [id] and return nothing
   Future<void> delete(String id) async {
-      await provider.collection(database + '-' + environment).doc(id).delete();
+    try {
+    await provider.collection(connectionString).doc(id).delete();
+    } on FirebaseException catch (e) {
+      throw RepositoryException(e.message!);
+    }
+  }
+
+  /// Update specific [fieldName] value of [T] identified by the [id] by a given [value]
+  /// Take a [T] and return nothing
+  Future<void> updateField(String id, String fieldName, dynamic value) async {
+    try {
+      await provider
+          .collection(connectionString)
+          .doc(id)
+          .update({fieldName: value});
+    } on FirebaseException catch (e) {
+      throw RepositoryException(e.message!);
+    }
   }
 
   /// Update a [T] object in the database
@@ -27,7 +47,7 @@ abstract class BaseAbstractRepository<T extends CargObject> {
   Future<void> update(T t) async {
     try {
       await provider
-          .collection(database + '-' + environment)
+          .collection(connectionString)
           .doc(t.id)
           .update(t.toJSON());
     } on FirebaseException catch (e) {
@@ -40,7 +60,7 @@ abstract class BaseAbstractRepository<T extends CargObject> {
   Future<String> create(T t) async {
     try {
       var documentReference = await provider
-          .collection(database + '-' + environment)
+          .collection(connectionString)
           .add(t.toJSON());
       return documentReference.id;
     } on FirebaseException catch (e) {
