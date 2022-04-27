45590b275caada4f87c41a34882eaa4c92cfc3e5
Valentin REVERSAT <dev@vareversat.fr>
2022-04-27 23:57:22 +0200

    clean(ctor): add type into ctors

--- lib/models/score/round/french_belote_round.dart
+++ lib/models/score/round/french_belote_round.dart
@@ -5,17 +5,17 @@ import 'package:enum_to_string/enum_to_string.dart';
 
 class FrenchBeloteRound extends BeloteRound {
   FrenchBeloteRound(
-      {index,
-      cardColor,
-      contractFulfilled,
-      dixDeDer,
-      beloteRebelote,
-      taker,
-      defender,
-      takerScore,
-      defenderScore,
-      usTrickScore,
-      themTrickScore})
+      {int? index,
+      CardColor? cardColor,
+      bool? contractFulfilled,
+      BeloteTeamEnum? dixDeDer,
+      BeloteTeamEnum? beloteRebelote,
+      BeloteTeamEnum? taker,
+      BeloteTeamEnum? defender,
+      int? takerScore,
+      int? defenderScore,
+      int? usTrickScore,
+      int? themTrickScore})
       : super(
             index: index,
             cardColor: cardColor,
