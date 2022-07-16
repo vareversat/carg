d242e6d3554a47dae6518ea0c488feee76aac74b
Valentin REVERSAT <dev@vareversat.fr>
2022-07-16 13:44:59 +0200

    feat(ui_redesign): redesign the TrickPointsBeloteWidget to be more user friendly + add some tests

--- lib/views/screens/add_round/widget/team_game/trick_points_belote_widget.dart
+++ lib/views/screens/add_round/widget/team_game/trick_points_belote_widget.dart
@@ -1,9 +1,7 @@
 import 'package:carg/models/score/misc/belote_team_enum.dart';
 import 'package:carg/models/score/round/belote_round.dart';
-import 'package:carg/styles/text_style.dart';
 import 'package:carg/views/screens/add_round/widget/section_title_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 class TrickPointsBeloteWidget extends StatefulWidget {
@@ -33,125 +31,164 @@ class _TrickPointsBeloteWidgetState extends State<TrickPointsBeloteWidget> {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider.value(
-      value: widget.round,
-      child: Consumer<BeloteRound>(
-          builder: (context, roundData, child) => Column(children: [
-                const SectionTitleWidget(title: 'Points des plis'),
-                Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Column(children: <Widget>[
-                      const SizedBox(height: 20),
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: <Widget>[
-                            Text('Nous',
-                                style: CustomTextStyle.boldAndItalic(context)),
-                            Text('Eux',
-                                style: CustomTextStyle.boldAndItalic(context))
-                          ]),
-                      Row(children: <Widget>[
-                        Flexible(
-                            child: TextField(
-                                controller: _usPointsTextController,
-                                style: const TextStyle(
-                                    fontSize: 20, fontWeight: FontWeight.bold),
-                                textAlign: TextAlign.center,
-                                keyboardType: TextInputType.number,
-                                inputFormatters: const <TextInputFormatter>[],
-                                onSubmitted: (String value) => {
-                                      roundData.usTrickScore = int.parse(value),
-                                      roundData.themTrickScore =
-                                          (BeloteRound.totalTrickScore -
-                                              int.parse(value)),
-                                      _themPointsTextController.text =
-                                          (BeloteRound.totalTrickScore -
-                                                  int.parse(value))
-                                              .toString(),
+        value: widget.round,
+        child: Consumer<BeloteRound>(
+            builder: (context, roundData, child) => Column(children: [
+                  const SectionTitleWidget(title: 'Points des plis'),
+                  Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Column(children: <Widget>[
+                        const SizedBox(height: 20),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: <Widget>[
+                              Text(
+                                  'Nous : ${roundData.usTrickScore.round().toString()} | '
+                                  '${(roundData.themTrickScore.round()).toString()} : Eux', key: const ValueKey('trickPointsBeloteRealTime'))
+                            ]),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Flexible(
+                                child: MaterialButton(
+                                    key: const ValueKey('trickPointsBeloteLeftButton'),
+                                    padding: EdgeInsets.zero,
+                                    onPressed: () {
+                                      roundData.usTrickScore > 0
+                                          ? roundData.usTrickScore--
+                                          : null;
+                                      roundData.themTrickScore <
+                                              BeloteRound.totalTrickScore
+                                          ? roundData.themTrickScore++
+                                          : null;
                                     },
-                                decoration: InputDecoration(
-                                    hintStyle: TextStyle(
-                                        fontSize: 20,
-                                        color: Theme.of(context).hintColor),
-                                    labelText: 'Points'))),
-                        const SizedBox(width: 20),
-                        Flexible(
-                            child: TextField(
-                                controller: _themPointsTextController,
-                                style: const TextStyle(
-                                    fontSize: 20, fontWeight: FontWeight.bold),
-                                textAlign: TextAlign.center,
-                                keyboardType: TextInputType.number,
-                                inputFormatters: const <TextInputFormatter>[],
-                                onSubmitted: (String value) => {
-                                      roundData.themTrickScore =
-                                          int.parse(value),
-                                      roundData.usTrickScore =
-                                          (BeloteRound.totalTrickScore -
-                                              int.parse(value)),
-                                      _usPointsTextController.text =
-                                          (BeloteRound.totalTrickScore -
-                                                  int.parse(value))
-                                              .toString(),
+                                    color: Theme.of(context).primaryColor,
+                                    textColor: Colors.white,
+                                    shape: const CircleBorder(),
+                                    child: const Icon(
+                                        Icons.chevron_left_outlined)),
+                              ),
+                              Flexible(
+                                flex: 5,
+                                child: Slider(
+                                  value: roundData.usTrickScore.toDouble(),
+                                  min: 0,
+                                  max: BeloteRound.totalTrickScore.toDouble(),
+                                  divisions: BeloteRound.totalTrickScore,
+                                  onChanged: (double value) {
+                                    roundData.usTrickScore = value.toInt();
+                                    roundData.themTrickScore =
+                                        BeloteRound.totalTrickScore -
+                                            value.toInt();
+                                  },
+                                ),
+                              ),
+                              Flexible(
+                                child: MaterialButton(
+                                    key: const ValueKey('trickPointsBeloteRightButton'),
+                                    onPressed: () {
+                                      roundData.usTrickScore <
+                                              BeloteRound.totalTrickScore
+                                          ? roundData.usTrickScore++
+                                          : null;
+                                      roundData.themTrickScore > 0
+                                          ? roundData.themTrickScore--
+                                          : null;
                                     },
-                                decoration: InputDecoration(
-                                    hintStyle: TextStyle(
-                                        fontSize: 20,
-                                        color: Theme.of(context).hintColor),
-                                    labelText: 'Points')))
-                      ]),
-                      const SizedBox(height: 10),
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            _BeloteRebeloteDixDeDerWidget(
-                                round: roundData, team: BeloteTeamEnum.US),
-                            _BeloteRebeloteDixDeDerWidget(
-                                round: roundData, team: BeloteTeamEnum.THEM)
-                          ])
-                    ]))
-              ])),
+                                    color: Theme.of(context).primaryColor,
+                                    textColor: Colors.white,
+                                    padding: EdgeInsets.zero,
+                                    shape: const CircleBorder(),
+                                    child: const Icon(
+                                        Icons.chevron_right_outlined)),
+                              ),
+                            ]),
+                        const SizedBox(height: 10),
+                        _BeloteRebeloteWidget(round: roundData),
+                        _DixDeDerWidget(round: roundData)
+                      ]))
+                ])));
+  }
+}
+
+class _BeloteRebeloteWidget extends StatelessWidget {
+  final BeloteRound? round;
+
+  const _BeloteRebeloteWidget({this.round});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        InputChip(
+            key: const ValueKey('beloteRebeloteWidgetUs'),
+            checkmarkColor: Theme.of(context).cardColor,
+            selected: round?.beloteRebelote == BeloteTeamEnum.US,
+            selectedColor: Theme.of(context).primaryColor,
+            onPressed: () => {
+                  if (round?.beloteRebelote != BeloteTeamEnum.US)
+                    round?.beloteRebelote = BeloteTeamEnum.US
+                  else
+                    round?.beloteRebelote = null
+                },
+            label: Text(BeloteTeamEnum.US.name,
+                style:
+                    TextStyle(fontSize: 20, color: Theme.of(context).cardColor),
+                overflow: TextOverflow.ellipsis)),
+        const Text('Belote / Rebelote'),
+        InputChip(
+            key: const ValueKey('beloteRebeloteWidgetThem'),
+            checkmarkColor: Theme.of(context).cardColor,
+            selected: round?.beloteRebelote == BeloteTeamEnum.THEM,
+            selectedColor: Theme.of(context).primaryColor,
+            onPressed: () => {
+                  if (round?.beloteRebelote != BeloteTeamEnum.THEM)
+                    round?.beloteRebelote = BeloteTeamEnum.THEM
+                  else
+                    round?.beloteRebelote = null
+                },
+            label: Text(BeloteTeamEnum.THEM.name,
+                style:
+                    TextStyle(fontSize: 20, color: Theme.of(context).cardColor),
+                overflow: TextOverflow.ellipsis)),
+      ],
     );
   }
 }
 
-class _BeloteRebeloteDixDeDerWidget extends StatelessWidget {
+class _DixDeDerWidget extends StatelessWidget {
   final BeloteRound? round;
-  final BeloteTeamEnum? team;
 
-  const _BeloteRebeloteDixDeDerWidget({this.round, this.team});
+  const _DixDeDerWidget({this.round});
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      Row(
-          textDirection:
-              team == BeloteTeamEnum.US ? TextDirection.ltr : TextDirection.rtl,
-          children: <Widget>[
-            const Text('Belote-Rebelote'),
-            Checkbox(
-                visualDensity: VisualDensity.compact,
-                value: round!.beloteRebelote == team,
-                onChanged: (bool? value) {
-                  if (value!) {
-                    round!.beloteRebelote = team;
-                  } else {
-                    round!.beloteRebelote = null;
-                  }
-                })
-          ]),
-      Row(
-          textDirection:
-              team == BeloteTeamEnum.US ? TextDirection.ltr : TextDirection.rtl,
-          children: <Widget>[
-            const Text('Dix de Der'),
-            Radio(
-                visualDensity: VisualDensity.compact,
-                value: team == BeloteTeamEnum.US,
-                onChanged: (bool? value) {
-                  round!.dixDeDer = team!;
-                },
-                groupValue: round!.dixDeDer == BeloteTeamEnum.US)
-          ])
-    ]);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        InputChip(
+            key: const ValueKey('dixDeDerWidgetUs'),
+            checkmarkColor: Theme.of(context).cardColor,
+            selected: round?.dixDeDer == BeloteTeamEnum.US,
+            selectedColor: Theme.of(context).primaryColor,
+            onPressed: () => {round?.dixDeDer = BeloteTeamEnum.US},
+            label: Text(BeloteTeamEnum.US.name,
+                style:
+                    TextStyle(fontSize: 20, color: Theme.of(context).cardColor),
+                overflow: TextOverflow.ellipsis)),
+        const Text('Dix de Der'),
+        InputChip(
+            key: const ValueKey('dixDeDerWidgetThem'),
+            checkmarkColor: Theme.of(context).cardColor,
+            selected: round?.dixDeDer == BeloteTeamEnum.THEM,
+            selectedColor: Theme.of(context).primaryColor,
+            onPressed: () => {round?.dixDeDer = BeloteTeamEnum.THEM},
+            label: Text(BeloteTeamEnum.THEM.name,
+                style:
+                    TextStyle(fontSize: 20, color: Theme.of(context).cardColor),
+                overflow: TextOverflow.ellipsis)),
+      ],
+    );
   }
 }
