b5a0ab41e06334cd2a7ca92c8c917a93721dabed
Valentin REVERSAT <dev@vareversat.fr>
2022-10-23 13:42:08 +0200

    feat(ads): create all model classes

---
+++ lib/models/iap/product/product_type_enum.dart
@@ -0,0 +1,4 @@
+enum ProductTypeEnum {
+  subscription,
+  nonSubscription,
+}
---
+++ lib/models/iap/purchase/non_subscription_purchase.dart
@@ -0,0 +1,48 @@
+import 'package:carg/models/iap/iap_source_enum.dart';
+import 'package:carg/models/iap/product/product_type_enum.dart';
+import 'package:carg/models/iap/purchase/non_subscription_status_enum.dart';
+import 'package:carg/models/iap/purchase/purchase.dart';
+import 'package:enum_to_string/enum_to_string.dart';
+
+class NonSubscriptionPurchase extends Purchase {
+  final NonSubscriptionStatusEnum status;
+
+  NonSubscriptionPurchase({
+    String? id,
+    required super.iapSource,
+    required super.orderId,
+    required super.productId,
+    required super.userId,
+    required super.purchaseDate,
+    required this.status,
+    super.type = ProductTypeEnum.nonSubscription,
+  }) : super(id: id);
+
+  @override
+  Map<String, dynamic> toJSON() {
+    final json = super.toJSON();
+    json.addAll({
+      'status': status.name,
+    });
+    return json;
+  }
+
+  factory NonSubscriptionPurchase.fromJSON(
+      Map<String, dynamic>? json, String id) {
+    return NonSubscriptionPurchase(
+        id: id,
+        iapSource: EnumToString.fromString(IAPSourceEnum.values, json?['iap_source'])!,
+        orderId: json?['order_id'],
+        productId: json?['product_id'],
+        userId: json?['user_id'],
+        purchaseDate: DateTime.parse(json?['purchase_date']),
+        type: EnumToString.fromString(ProductTypeEnum.values, json?['type'])!,
+        status: EnumToString.fromString(
+            NonSubscriptionStatusEnum.values, json?['status'])!);
+  }
+
+  @override
+  bool isValid() {
+    return status == NonSubscriptionStatusEnum.purchased;
+  }
+}
