bc7c41e079433a33a08221292c7732dc09bd92a1
Valentin REVERSAT <dev@vareversat.fr>
2025-05-18 02:30:01 +0200

    chore(deps): upgrade Firebase functions deps (#535)

--- firebase_functions/functions/src/index.ts
+++ firebase_functions/functions/src/index.ts
@@ -1,82 +1,62 @@
 import * as functions from 'firebase-functions';
-import algoliasearch from 'algoliasearch';
 import * as firestore from "@google-cloud/firestore";
+import { algoliasearch } from 'algoliasearch';
 
 import * as algolia_key from "./assets/algolia-key.json";
 import * as google_key from "./assets/backup-service-key.json";
 
 const algoliaClient = algoliasearch(algolia_key.app_id, algolia_key.api_key);
-const firestoreClient = new firestore.v1.FirestoreAdminClient({credentials:
-    {client_email: google_key.client_email, private_key: google_key.private_key }
+const firestoreClient = new firestore.v1.FirestoreAdminClient({
+    credentials:
+        { client_email: google_key.client_email, private_key: google_key.private_key }
 });
-const indexDev = algoliaClient.initIndex('player-dev');
-const indexProd = algoliaClient.initIndex('player-prod');
+
+const indexDev = 'player-dev';
+const indexProd = 'player-prod';
 
 
 // App functions
-export const onPlayerCreatedDev = functions.firestore.document('player-dev/{playerId}').onCreate((snap, context) => {
-    const player = snap.data();
+export const onPlayerWrittenDev = functions.firestore.onDocumentWritten('player-dev/{playerId}', (event: any) => {
+    const player = event.data.after.data();
     if (player !== undefined) {
-        player.objectID = context.params.playerId;
-        return indexDev.saveObject(player);
+        return algoliaClient.addOrUpdateObject({ indexName: indexDev, body: player, objectID: event.params.playerId });
+    } else {
+        return algoliaClient.deleteObject({ indexName: indexDev, objectID: event.params.playerId });
     }
-    return 'Cannot add to index'
-
 });
 
-export const onPlayerUpdatedDev = functions.firestore.document('player-dev/{playerId}').onUpdate((snap, context,) => {
-    const newPlayer = snap.after.data();
-    if (newPlayer !== undefined) {
-        newPlayer.objectID = context.params.playerId;
-        return indexDev.saveObject(newPlayer);
-    }
-    return 'Cannot update index'
-
-});
-
-export const onPlayerDeletedDev = functions.firestore.document('player-dev/{playerId}').onDelete((snap, context,) => {
-    return indexDev.deleteObject(context.params.playerId);
-});
-
-export const onPlayerCreatedProd = functions.firestore.document('player-prod/{playerId}').onCreate((snap, context) => {
-    const player = snap.data();
+export const onPlayerWrittenProd = functions.firestore.onDocumentWritten('player-prod/{playerId}', (event: any) => {
+    const player = event.data.after.data();
     if (player !== undefined) {
-        player.objectID = context.params.playerId;
-        return indexProd.saveObject(player);
+        return algoliaClient.addOrUpdateObject({ indexName: indexProd, body: player, objectID: event.params.playerId });
+    } else {
+        return algoliaClient.deleteObject({ indexName: indexProd, objectID: event.params.playerId });
     }
-    return 'Cannot add to index'
-
-});
-
-export const onPlayerUpdatedProd = functions.firestore.document('player-prod/{playerId}').onUpdate((snap, context,) => {
-    const newPlayer = snap.after.data();
-    if (newPlayer !== undefined) {
-        newPlayer.objectID = context.params.playerId;
-        return indexProd.saveObject(newPlayer);
-    }
-    return 'Cannot update index'
-
-});
-
-export const onPlayerDeletedProd = functions.firestore.document('player-prod/{playerId}').onDelete((snap, context,) => {
-    return indexProd.deleteObject(context.params.playerId);
 });
 
 // Backup functions
+export const backupFirestore = functions.scheduler.onSchedule({ schedule: 'every day 00:00', timeZone: 'Europe/Paris' }, async (event: any) => {
 
-export const backupFirestore = functions.pubsub.schedule('every day 00:00').timeZone('Europe/Paris').onRun(async (context) => {
-    const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT || '';
-    const bucketName = `${projectId}-firestore-backup`;
-    const databaseName = firestoreClient.databasePath(projectId,'(default)');
-    const timestamp = new Date().toISOString();
+    try {
+        const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT || '';
+        const bucketName = `${projectId}-firestore-backup`;
+        const databaseName = firestoreClient.databasePath(projectId, '(default)');
+        const timestamp = new Date().toISOString();
 
-    console.log(`Local starting time is ${timestamp}`);
-    console.log(`Start to backup project ${projectId}`);
+        console.log(`Local starting time is ${timestamp}`);
+        console.log(`Start to backup project ${projectId}`);
 
-    return firestoreClient.exportDocuments({
-        name: databaseName,
-        outputUriPrefix: `gs://${bucketName}/backups/${timestamp}`,
-        collectionIds: []
-    });
+        const responses = await firestoreClient.exportDocuments({
+            name: databaseName,
+            outputUriPrefix: `gs://${bucketName}/backups/${timestamp}`,
+            collectionIds: []
+        });
+
+        console.log(`Backup completed: ${responses[0].error}`);
+    } catch (error) {
+        console.error("Error during Firestore backup:", error);
+        // Optionally, you can throw the error to mark the function execution as failed
+        throw error;
+    }
 
 });
